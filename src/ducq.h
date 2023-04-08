a48a195c8ec75e4fbe89b9d5f09fe5119e343b35
Sebastien Bedard <ducq.core@gmail.com>
2023-04-08 10:55:20 -0400

    cleaned up interface vs vtable

--- src/ducq.h
+++ src/ducq.h
@@ -30,30 +30,17 @@ const char *ducq_state_tostr(int state);
 
 
 
+typedef struct ducq_i ducq_i;
 
-typedef struct ducq_t ducq_t;
-
-ducq_state  ducq_conn(ducq_t *ducq);
-ducq_state  ducq_send(ducq_t *ducq, void *buf, size_t *size);
-ducq_state  ducq_recv(ducq_t *ducq, void *buf, size_t *size);
-const char *ducq_id  (ducq_t *ducq);
-bool        ducq_eq  (ducq_t *a, ducq_t *b);
-ducq_t     *ducq_copy(ducq_t *ducq);
-ducq_state  ducq_timeout(ducq_t *ducq, int timeout);
-ducq_state  ducq_close(ducq_t *ducq);
-void        ducq_free (ducq_t *ducq);
-
-
-
-// ducq_state ducq_emit(ducq_t *ducq, const char *command, const char *route, const char *payload);
-
-typedef ducq_state (*ducq_apply_f)(void *cl, ducq_t *ducq);
-
-
-// sugar
-typedef int (*on_msg_f)(const char *payload, size_t size, void *cl);
-ducq_state ducq_subscribe(ducq_t *ducq, const char *route, on_msg_f onmsg, void *cl);
-ducq_state ducq_publish  (ducq_t *ducq, const char *route, const char *payload);
+ducq_state  ducq_conn(ducq_i *ducq);
+ducq_state  ducq_send(ducq_i *ducq, void *buf, size_t *size);
+ducq_state  ducq_recv(ducq_i *ducq, void *buf, size_t *size);
+const char *ducq_id  (ducq_i *ducq);
+bool        ducq_eq  (ducq_i *a, ducq_i *b);
+ducq_i     *ducq_copy(ducq_i *ducq);
+ducq_state  ducq_timeout(ducq_i *ducq, int timeout);
+ducq_state  ducq_close(ducq_i *ducq);
+void        ducq_free (ducq_i *ducq);
 
 
 
